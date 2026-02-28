0ed383c559e6e0dc3d36cc6589c43c8dbefc0afb
mhluck <moh.luqman.noer@gmail.com>
2026-02-28 23:59:31 +0700

    mencoba mengisi database menggunakan tinker dengan membuat model dahulu

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,14 +1,12 @@
 <?php
 
 namespace App\Models;
-//use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-//use Illuminate\Support\Arr;
 
 class Post extends Model
 {
-    //use HasFactory;
-
-    protected $table = 'blog_posts';
-    protected $primaryKey = 'id';
+    use HasFactory;
+    protected $fillable = ['title', 'slug', 'author', 'content'];
 }
