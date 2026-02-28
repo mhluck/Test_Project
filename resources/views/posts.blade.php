cebeff352f7a5e65883e361ccb66965efa2d9e5d
mhluck <moh.luqman.noer@gmail.com>
2026-02-28 16:59:54 +0700

    update slug

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,14 +3,14 @@
 
     @foreach ($posts as $post)
         <article class="py-8 max-w-3xl border-b border-gray-300">
-            <a href="/posts/{{ $post['id'] }}" class="hover:underline">
+            <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
             </a>
             <div class="text-base text-gray-500">
                 <a href="#">{{ $post['author'] }}</a> | 12 June 2024
             </div>
             <p class="my-4 font-light">{{ str($post['content'])->limit(150) }}</p>
-            <a href="/posts/{{ $post['id'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+            <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
         </article>
     @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog Page', 'posts' => Post::all()]);
 });
 
-route::get('posts/{id}', function ($id) {
+route::get('posts/{post:slug}', function (Post $post) {
 
-    $post = Post::find($id);
+    // $post = Post::find($id);
+
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
