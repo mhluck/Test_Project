021eb32de71f76202f92c4ba2782a6a8f9d4c4ac
mhluck <moh.luqman.noer@gmail.com>
2026-02-28 16:42:46 +0700

    intial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Welcome to the About Page!</h3>
+    <P>Hi, my name is {{ $nama }}.</P>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Post;
+//use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => Post::all()]);
+});
+
+route::get('posts/{id}', function ($id) {
+
+    $post = Post::find($id);
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'HEYLUCK', 'title' => 'About Page']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact Page']);
+});
